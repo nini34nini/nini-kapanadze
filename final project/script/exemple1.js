a1ed70f3aeb938aa87e617f30897ce7909baf9de
nini kapanadze <kapanadzenini54@gmail.com>
2023-11-11 16:51:33 +0400

    html, css, final project

---
+++ final project/script/exemple1.js
@@ -0,0 +1,12 @@
+const menuBtn = document .querySelector('.menu-btn')
+const navlink = document .querySelector('nav-link')
+
+menuBtn.addEventListener('click',()=>{
+    navlink.classList.toggle('mobile-menu')
+    if(nav-links.style.disply === block){
+        navlink.style.disply = "none";
+    } else{
+        navlink.style.disply = "block";
+    }
+
+})
